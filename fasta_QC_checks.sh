64002e3fdc2779e378f27817263cd750a193839b
Maggie Chen <ychen254@dons.usfca.edu>
2018-09-21 23:40:23 +0000

    Indicate the log file and provide the date

---
+++ fasta_QC_checks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This is script to produce a summary of information
+# about each of the fasta file
+
+# Maggie Chen
+# Spetember 21, 2018
+# ychen254@dons.usfca.edu
+
+# In order to use this script to get information about fasta files
+# please unzip them, thus they are in the form of .fasta
+
+# This will indicate the output is a log file, and  include the date
+echo "Output from preliminary quality control checks on selected fasta files"
+date
+
